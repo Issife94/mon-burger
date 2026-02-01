9b9eff5de1795390923d580ac08635e1c6449a75
Issife94 <issife@hotmail.fr>
2026-02-01 15:26:11 +0100

    🎉 Apps.js > Add Hello wolrd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          salut issife
+          Hello world
         </p>
         <a
           className="App-link"
