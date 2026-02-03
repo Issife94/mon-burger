d38f2a964f80c48881019f6af7085a3aabc42be4
Issife94 <issife@hotmail.fr>
2026-02-03 09:59:44 +0100

    ✨F01: LOGIN > Create LoginPage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import LoginPage from "./components/pages/LoginPage";
+
 function App() {
   //state (état, donnée)
 
   //comportements
 
   //affichage (render)
-  return 
+  return <LoginPage />
 }
 export default App;
