54d4172d5aa3241302f78fb96eff446e82cb0b80
Issife94 <issife@hotmail.fr>
2026-02-05 14:14:38 +0100

    💄F03-live: 4 m"thodes pour ajouter du style

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import "./LoginForm.css"
 
 export default function LoginForm() {
   //state (état, donnée)
@@ -12,12 +13,17 @@ export default function LoginForm() {
     navigate(`order/${inputValue}`);
   }
   const handleChange = (e) => { setInputValue(e.target.value) }
+
+  const titreH1 = {
+    backgroundColor: "red", color: "white", fontSize: 24
+  }
+
   //affichage
   return (
     <form action="submit" onSubmit={handleSubmit}>
-      <h1>Bienvenue chez nous !</h1>
+      <h1 style={titreH1}>Bienvenue chez nous !</h1>
       <br />
-      <h2>Connectez-vous</h2>
+      <h2 className="bonbon">Connectez-vous</h2>
       <input
         value={inputValue}
         onChange={handleChange}
@@ -29,3 +35,12 @@ export default function LoginForm() {
     </form>
   )
 }
+
+
+/** 
+ * 4 méthodes pour ajouter du style à un composant 
+ * 1. inline style -> ajouter un cas en guise de test
+ * 2. global style -> utile pour les conditional rendering
+ * 3. modules CSS (avec className) -> un autre fichier qu'on importera
+ * 4. global style (index.css) -> les règles 
+ * **/
